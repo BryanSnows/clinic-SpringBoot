5d818f4a43ac1ae63959f7081637153f4ddd0841
bryan neves pinto <bryan.pinto@conecthus.org.br>
2023-11-13 13:23:51 -0400

    feat: create swagger config

--- src/main/java/com/example/clinic/repositories/ProductRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.clinic.repositories;
-import com.example.clinic.models.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
